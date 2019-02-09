eb0da4b4d7b895c004aa3bf154a1471066c71cb5
Mohd Shahid <mshahid85@gmail.com>
2019-02-10 01:31:35 +0530

    adds fff

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
 
-var request = require('supertest');
-var app = require('../index.js');
+var request = require( 'supertest' );
+var app = require( '../index.js' );
 
-describe('GET /', function() {
- it('respond with hello world', function(done) {
- //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello world', done);
- });
-});
+describe( 'GET /', function () {
+    it( 'respond with hello world', function ( done ) {
+        //navigate to root and check the the response is "hello world"
+        request( app ).get( '/' ).expect( 'hello world', done );
+    } );
+} );
